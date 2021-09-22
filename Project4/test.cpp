633b20419268ac96f5b242e6d22e7451afaee607
xjh <xihiverson@outlook.com>
2021-09-22 09:30:19 +0800

    扫雷

---
+++ Project4/test.cpp
@@ -0,0 +1,65 @@
+#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS 1
+#include "game.h"
+
+
+
+
+void meun() {
+	printf("***************\n");
+	printf("****1.play*****\n");
+	printf("****0.exit*****\n");
+	printf("***************\n");
+
+
+}
+void game() {
+	char mine[ROWS][COLS] = { 0 };
+	char show[ROWS][COLS] = { 0 };
+	InitBoard(mine, ROWS, COLS, '0');
+	InitBoard(show, ROWS, COLS, '*');
+	DisplayBoard(mine,ROW,COL);
+	DisplayBoard(show, ROW, COL);
+	SetMine(mine,ROW,COL);
+	DisplayBoard(mine, ROW, COL);
+	FindMine(mine,show, ROW, COL);
+
+	printf("扫雷\n");
+
+}
+
+void test() {
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do {
+		meun();
+		printf("请选择");
+
+		scanf_s("%d", &input);
+		switch (input)
+		{
+		case 1:
+			game();
+			break;
+		case 0:
+			printf("退出游戏\n");
+			break;
+		default:
+			printf("请重新选择\n");
+
+			break;
+		}
+		
+	} while (input);
+	
+
+}
+
+
+
+
+int main() {
+	test();
+	return 0;
+	
+}
